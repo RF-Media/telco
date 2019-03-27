28287a7bdbf414a7573c9537efd88d0438e6e751
roy <roy@syndrome.co.il>
2019-03-27 16:24:46 +0400

    Updates

---
+++ precache-manifest.759fb496a21dab004880f2ab7dd61af6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f73aa07f0a6f338024510f8565bf9ee1",
+    "url": "/telco/static/media/welcome-background.f73aa07f.png"
+  },
+  {
+    "revision": "d34354db4a860869b846",
+    "url": "/telco/static/js/runtime~main.13a1d265.js"
+  },
+  {
+    "revision": "c086f400044a40ea892a",
+    "url": "/telco/static/js/main.ff8bd67e.chunk.js"
+  },
+  {
+    "revision": "2496fd16a2f297e29ddc",
+    "url": "/telco/static/js/2.ff73bca5.chunk.js"
+  },
+  {
+    "revision": "1bd68f50aad628a76a792479ea61412b",
+    "url": "/telco/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.231785762aeeac62aa868a52ea3f4214.js"
+  "/telco/precache-manifest.759fb496a21dab004880f2ab7dd61af6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/telco/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
