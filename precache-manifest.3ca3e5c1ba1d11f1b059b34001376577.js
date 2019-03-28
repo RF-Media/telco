d5eee8cf418c8c9ffcb0939e43118c56844561e2
roy <roy@syndrome.co.il>
2019-03-28 22:40:50 +0400

    Updates

---
+++ precache-manifest.3ca3e5c1ba1d11f1b059b34001376577.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "842071405144d56adae83cf14efd6250",
+    "url": "/static/media/feature-1.84207140.svg"
+  },
+  {
+    "revision": "8dc5d0215b4b79273e8f",
+    "url": "/static/js/main.f18991cd.chunk.js"
+  },
+  {
+    "revision": "f73aa07f0a6f338024510f8565bf9ee1",
+    "url": "/static/media/welcome-background.f73aa07f.png"
+  },
+  {
+    "revision": "ea08f70ae7558a3a9b66cd9b78a898c4",
+    "url": "/static/media/sip.ea08f70a.svg"
+  },
+  {
+    "revision": "dfd66a16cf8c546a2394593f4b3fc21c",
+    "url": "/static/media/mobile.dfd66a16.svg"
+  },
+  {
+    "revision": "5a142b4a199fd5e6b314d79dbd87822d",
+    "url": "/static/media/feature-2.5a142b4a.svg"
+  },
+  {
+    "revision": "80c89b122473849df05dceabe120ea38",
+    "url": "/static/media/feature-4.80c89b12.svg"
+  },
+  {
+    "revision": "032edb9909770a12d4825889608b1dbb",
+    "url": "/static/media/feature-8.032edb99.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a4bcec4721da46c58f6517d18fba69e0",
+    "url": "/static/media/feature-3.a4bcec47.svg"
+  },
+  {
+    "revision": "1f32d817b9480ec11e33a94e7cdc5ab4",
+    "url": "/static/media/feature-5.1f32d817.svg"
+  },
+  {
+    "revision": "1214937256d1944b5540af48095b380a",
+    "url": "/static/media/feature-6.12149372.svg"
+  },
+  {
+    "revision": "935e36dbe6bb76cef58ee3789488839d",
+    "url": "/static/media/feature-7.935e36db.svg"
+  },
+  {
+    "revision": "60e28aaadcf8114c1769141a65d703ca",
+    "url": "/static/media/feature-9.60e28aaa.svg"
+  },
+  {
+    "revision": "63331cb19a432245e12b",
+    "url": "/static/js/2.4ad11944.chunk.js"
+  },
+  {
+    "revision": "206e947b30cc5d56b7f0429e3c6e0fcb",
+    "url": "/index.html"
+  }
+];
